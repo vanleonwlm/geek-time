719788f7cfa7f31e6d3cd777afe7358ae3016a14
vanleon <125458062+vanleonwlm@users.noreply.github.com>
2024-02-29 10:15:54 +0800

    add public columns and remove auth by session

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const staticMiddleware = require('./static.middleware');
-const authMiddleware = require('./auth.middleware');
 
 app.use(cookieParser());
 app.use(staticMiddleware);
-app.use(authMiddleware);
 
 module.exports = app;
