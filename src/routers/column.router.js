b4cb3f59112598203c3711e31aeb43e6a890747a
vanleonwlm <79657392@qq.com>
2025-08-02 20:03:23 +0800

    add login button

--- src/routers/column.router.js
+++ src/routers/column.router.js
@@ -2,15 +2,22 @@ import express from 'express';
 import columnService from '../services/column.service.js';
 import nunjucks from '../configs/nunjucks.config.js';
 import {parseInt, redirectErrorPage} from '../utils/common.utils.js';
+import env from "../configs/env.config.js";
 
 const router = express.Router();
 
 router.get(['/columns', '/'], async (req, res) => {
+    const oauth = {
+        isLogin: !!req.session.user,
+        loginUrl: `https://github.com/login/oauth/authorize?client_id=${env.github.oauth.clientId}&redirect_uri=${req.protocol}://${req.get('host')}/github/oauth/redirect`
+    };
+
     let columns = await columnService.list();
     columns = columns.filter(column => {
         return columnService.canBrowseColumn(req, res, column.id);
     });
-    const html = nunjucks.render('columns.html', {columns});
+
+    const html = nunjucks.render('columns.html', {oauth, columns});
     res.send(html);
 });
 
